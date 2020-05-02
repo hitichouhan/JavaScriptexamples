3a5662306dbace2d8b11dfd4dd61b0178c17dea3
hitichouhan <35139014+hitichouhan@users.noreply.github.com>
2020-05-02 13:33:31 -0700

    Add files via upload

---
+++ Calculate BMI.js
@@ -0,0 +1,28 @@
+/*****************************
+* CODING PROBLEM 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+
+*/
+/*
+var massMark = 70; // kg ;store mass
+var heightMark = 1.69; // meters ; store height 
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark); // calculate BMI
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+*/
